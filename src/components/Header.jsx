01075b9189fbd9d3047402b5c80f4fc17f65fe56
edgarguitarist <edgarguitarist@gmail.com>
2023-12-19 00:27:23 -0500

    feat: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 export default function Header() {
   return (
-    <div className="bg-white dark:bg-slate-800  px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
-        <h1 className="text-3xl font-bold text-slate-900 dark:text-white">
-            ChessBoard FBIM!
-        </h1>
+    <div className="bg-white h-[10vh] dark:bg-slate-800  px-6 py-4 ring-1 ring-slate-900/5 shadow-xl">
+      <h1 className="text-3xl font-bold text-slate-900 dark:text-white">
+        ChessBoard
+      </h1>
     </div>
   );
 }
