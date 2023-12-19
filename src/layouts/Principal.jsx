69888a03f9d9707023de9a9a1c9f68c14a32c19b
edgarguitarist <edgarguitarist@gmail.com>
2023-12-19 00:28:46 -0500

    feat: principal layout

---
+++ src/layouts/Principal.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Principal({ children }) {
+    
+    return (
+        <div className="grid grid-cols-3 h-[90vh] place-content-center ">
+            {children}
+        </div>
+    )
+}
