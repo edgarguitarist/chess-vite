9d50ef55144719ccb6e363354e10bed430af1111
edgarguitarist <edgarguitarist@gmail.com>
2023-12-19 00:28:32 -0500

    feat: board logic

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,51 +1,28 @@
-import { defaultPieces } from "../media/pieces";
-export default function Board() {
-  //const board = Array(8).fill(Array(8).fill(null));
-  const board = [
-    ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
-    ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
-    [null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null],
-    ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
-    ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
-  ];
-  const handleClick = (coords) => {
-    console.log(coords);
-  };
+import { useBoardStore } from "../store/BoardStore";
+import Square from "./Square";
 
-  const Square = ({ children, coords }) => {
-    const color =
-      (coords[0] + coords[1]) % 2 === 0 ? "bg-white" : "bg-green-900";
-    console.log(children);
-    return (
-      <>
-        <div
-          className={color + " square w-12 h-12  flex"}
-          onClick={() => handleClick(coords)}
-        >
-          {children ? defaultPieces[children] : null}
-        </div>
-      </>
-    );
-  };
+export default function Board() {
+  const { board } = useBoardStore();
 
   return (
-    <div className="border-2 border-black w-fit">
-      {board.map((row, rowIndex) => {
-        return (
-          <div className="grid grid-cols-8 gap-0 w-fit" key={rowIndex}>
-            {row.map((piece, colIndex) => {
-              return (
-                <Square key={colIndex} coords={[rowIndex, colIndex]}>
-                  {piece}
-                </Square>
-              );
-            })}
-          </div>
-        );
-      })}
-    </div>
+    <section className="grid place-content-center h-[80vh]">
+      <div className="border-2 border-black w-fit">
+        {board.map((row, rowIndex) => {
+          return (
+            <div className="grid grid-cols-8 gap-0 w-fit" key={rowIndex}>
+              {row.map((piece, colIndex) => {
+                return (
+                  <Square
+                    key={colIndex}
+                    coords={[rowIndex, colIndex]}
+                    piece={{ ...piece, coords: [rowIndex, colIndex] }}
+                  />
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </section>
   );
 }
