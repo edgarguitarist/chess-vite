b93ef4adcabdfd42415d4c6d3e53b50e0f91b47d
edgarguitarist <edgarguitarist@gmail.com>
2023-12-26 01:13:17 -0500

    feat: informacion del juego

--- src/components/GameInfo.jsx
+++ src/components/GameInfo.jsx
@@ -1,13 +1,29 @@
 import { useBoardStore } from "../store/BoardStore";
+import { PIECES } from "../media/pieces";
+import { TURNS } from "../types/Piece";
 
 export default function GameInfo() {
-    const {turn} = useBoardStore();
-    console.log({ turn });
+  const { turn, blackDefeatedPieces, whiteDefeatedPieces } = useBoardStore();
+  console.log({ turn });
   return (
     <div className="grid grid-rows-3">
-      <div>fichas negras perdidas</div>
-      <div>Es el turno de las {turn}</div>
-      <div>fichas blancas perdidas</div>
+      <div>Es el turno de los {turn}</div>
+      <div>
+        Fichas {TURNS.BLACK} derrotadas
+        <div className="grid grid-cols-8">
+          {blackDefeatedPieces.map((piece, index) => (
+            <div key={index}>{PIECES[piece.name]}</div>
+          ))}
+        </div>
+      </div>
+      <div>
+        Fichas {TURNS.WHITE} derrotadas
+        <div className="grid grid-cols-8">
+          {whiteDefeatedPieces.map((piece, index) => (
+            <div key={index}>{PIECES[piece.name]}</div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
