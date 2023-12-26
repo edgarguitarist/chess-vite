090a0f74c776023902980d082de17ef3ec91c2a4
edgarguitarist <edgarguitarist@gmail.com>
2023-12-26 01:12:45 -0500

    feat: referencias de coordenadas

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,18 +3,41 @@ import Square from "./Square";
 
 export default function Board() {
   const { board } = useBoardStore();
-
   return (
     <section className="grid place-content-center h-[80vh]">
-      <div className="border-2 border-black w-fit">
+      <div className="border-2 border-black w-fit grid grid-rows-9 gap-0">
         {board.map((row, rowIndex) => {
+          if (rowIndex === 0) {
+            return (
+              <div className="grid cols-board gap-0 w-full" key={rowIndex}>
+                {row.map((piece, colIndex) => {
+                  if (colIndex === 0) {
+                    return <div key={piece}></div>;
+                  }
+                  return (
+                    <div
+                      className="bg-gray-300 text-center"
+                      key={colIndex}
+                    >{piece}</div>
+                  );
+                })}
+              </div>
+            );
+          }
           return (
-            <div className="grid grid-cols-8 gap-0 w-fit" key={rowIndex}>
+            <div className="grid cols-board gap-0" key={rowIndex}>
               {row.map((piece, colIndex) => {
+                if (colIndex === 0) {
+                  return (
+                    <div
+                      className="bg-gray-300 text-center w-full h-full grid  place-items-center"
+                      key={colIndex}
+                    >{piece}</div>
+                  )
+                }
                 return (
                   <Square
                     key={colIndex}
-                    coords={[rowIndex, colIndex]}
                     piece={{ ...piece, coords: [rowIndex, colIndex] }}
                   />
                 );
