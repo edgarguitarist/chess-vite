a16b474d1d7a1c466fff22ddf6d3fafdc61f18dd
edgarguitarist <edgarguitarist@gmail.com>
2024-01-03 07:23:31 -0500

    fix: Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 export default function Header() {
   return (
-    <div className="bg-white h-[10vh] dark:bg-slate-800  px-6 py-4 ring-1 ring-slate-900/5 shadow-xl">
-      <h1 className="text-3xl font-bold text-slate-900 dark:text-white">
-        ChessBoard
-      </h1>
+    <div className="h-[10vh] bg-slate-800 ring-1 ring-slate-900/5 shadow-xl flex justify-between items-center px-10">
+      <span className="text-3xl font-bold text-white">
+        AJEDREZ
+      </span>
+      <button type="button" className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200">COMENZAR PARTIDA</button>
     </div>
   );
 }
