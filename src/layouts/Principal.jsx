83419f3316fc9d9d0cad1e96e398536add02b99c
edgarguitarist <edgarguitarist@gmail.com>
2024-01-03 07:26:44 -0500

    fix: style margin

--- src/layouts/Principal.jsx
+++ src/layouts/Principal.jsx
@@ -3,7 +3,7 @@
 export default function Principal({ children }) {
     
     return (
-        <div className="grid grid-cols-3 h-[90vh] place-content-center ">
+        <div className="grid grid-cols-3 h-[90vh] place-content-center mx-5">
             {children}
         </div>
     )
