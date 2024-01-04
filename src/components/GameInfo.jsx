f5289b287236ad014024ea0a967f8be7d086edf2
edgarguitarist <edgarguitarist@gmail.com>
2024-01-04 06:26:29 -0500

    fix: game history location

--- src/components/GameInfo.jsx
+++ src/components/GameInfo.jsx
@@ -1,29 +1,20 @@
 import { useBoardStore } from "../store/BoardStore";
 import { PIECES } from "../media/pieces";
 import { TURNS } from "../types/Piece";
-import DefeatedPieces from "./DefeatedPieces";
+import GameHistory from "./GameHistory";
 
 export default function GameInfo() {
-  const { turn, blackDefeatedPieces, whiteDefeatedPieces } = useBoardStore();
+  const { turn } = useBoardStore();
   const PIECE = turn === TURNS.WHITE ? PIECES["wK"] : PIECES["bK"];
 
   return (
-    <div className="grid game-info p-5">
+    <div className="grid grid-max-content p-5">
       <div className="border-2 rounded border-black mb-6">
         <span className="text-2xl font-semibold flex items-center place-content-center my-2">
           Es el turno de las {turn} {PIECE}
         </span>
       </div>
-      <div className="grid grid-rows-2 gap-5">
-        <DefeatedPieces
-          color={TURNS.BLACK}
-          storedDefeatedPieces={blackDefeatedPieces}
-        ></DefeatedPieces>
-        <DefeatedPieces
-          color={TURNS.WHITE}
-          storedDefeatedPieces={whiteDefeatedPieces}
-        ></DefeatedPieces>
-      </div>
+      <GameHistory />
     </div>
   );
 }
