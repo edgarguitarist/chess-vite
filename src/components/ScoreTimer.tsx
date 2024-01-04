d80318bf2a60f903ff6db620c05f5b5a1449a1b9
edgarguitarist <edgarguitarist@gmail.com>
2024-01-04 06:24:47 -0500

    fix: score style

--- src/components/ScoreTimer.tsx
+++ src/components/ScoreTimer.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 export default function ScoreTimer() {
-    return (
-        <div className="flex justify-between text-xl font-semibold my-4">
-        <span>SCORE - 500</span>
-        <span>09:50</span>
-      </div>
-    )
+  return (
+    <div className="flex justify-between text-xl font-semibold my-4 bg-slate-800 text-white px-5 rounded-xl py-0.5">
+      <span>SCORE - 500</span>
+      <span>09:50</span>
+    </div>
+  );
 }
