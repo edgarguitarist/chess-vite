d0825cc8fe3623465603f42e56ab7dbef5873db4
edgarguitarist <edgarguitarist@gmail.com>
2024-01-04 06:24:25 -0500

    fix: selected piece style

--- src/components/SelectedPiece.tsx
+++ src/components/SelectedPiece.tsx
@@ -5,25 +5,34 @@ import { useBoardStore } from "../store/BoardStore";
 import { Piece } from "../types/Piece";
 
 export default function SelectedPiece() {
-const { selectedSquare } = useBoardStore() as { selectedSquare: Piece };
+  const { selectedSquare } = useBoardStore() as { selectedSquare: Piece };
 
   return (
-    <div className="text-2xl font-semibold p-5 border-2 rounded border-black">
-      <div className="w-full flex items-end gap-4 h-[45px]">
-        Pieza seleccionada:{" "}
-        {!selectedSquare.name ? "N/A" : PIECES[selectedSquare.name]}
+    <div className="text-xl font-semibold border-2 rounded border-black">
+      <div>
+        <span className="text-2xl p-2 block text-center font-semibold bg-slate-800 text-white">
+          Pieza seleccionada
+        </span>
       </div>
-      <div className="w-full flex items-center gap-4">
-        Nombre: {!selectedSquare.name ? "N/A" : getPieceName(selectedSquare)}
-      </div>
-      <div className="w-full flex items-center gap-4">
-        Color: {!selectedSquare.color ? "N/A" : selectedSquare.color}
-      </div>
-      <div className="w-full flex items-center gap-4">
-        Coordenadas:{" "}
-        {selectedSquare.coords[0] === 0
-          ? "N/A"
-          : realCoords(selectedSquare.coords)}
+      <div className="py-2 px-5 grid grid-piece-info">
+        <div>
+          <div className="w-full flex items-center gap-4">
+            Nombre:{" "}
+            {!selectedSquare.name ? "N/A" : getPieceName(selectedSquare)}
+          </div>
+          <div className="w-full flex items-center gap-4">
+            Color: {!selectedSquare.color ? "N/A" : selectedSquare.color}
+          </div>
+          <div className="w-full flex items-center gap-4">
+            Coordenadas:{" "}
+            {selectedSquare.coords[0] === 0
+              ? "N/A"
+              : realCoords(selectedSquare.coords)}
+          </div>
+        </div>
+        <div className="grid place-items-center scale-110">
+          {!selectedSquare.name ? "N/A" : PIECES[selectedSquare.name]}
+        </div>
       </div>
     </div>
   );
