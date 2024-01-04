64ae7028225934a44c3136745e146d5e95d38ea0
edgarguitarist <edgarguitarist@gmail.com>
2024-01-04 06:26:13 -0500

    fix: header button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
       <span className="text-3xl font-bold text-white">
         AJEDREZ
       </span>
-      <button type="button" className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200">COMENZAR PARTIDA</button>
+      <button type="button" className="py-2.5 px-5 text-sm font-medium text-gray-900 bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 hover:scale-105">COMENZAR PARTIDA</button>
     </div>
   );
 }
