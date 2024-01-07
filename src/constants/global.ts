1595496cbe23ea19f513e10a23b595051e900f7e
edgarguitarist <edgarguitarist@gmail.com>
2024-01-06 19:15:10 -0500

    update: types

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -5,7 +5,6 @@ const createDefaultPiece = (color, name):Piece => {
     name,
     color,
     coords: [9, 9],
-    defeated: false,
     moveSet: null,
   }
 }
@@ -24,37 +23,20 @@ const wK = createDefaultPiece(TURNS.WHITE, "wK")
 const wP = createDefaultPiece(TURNS.WHITE, "wP")
 
 export const defaultBoard = [
-  [1, wR, wN, wB, wQ, wK, wB, wN, wR].flat(),
-  [2, Array(8).fill(wP)].flat(),
-  [3, Array(8).fill(null)].flat(),
-  [4, Array(8).fill(null)].flat(),
-  [5, Array(8).fill(null)].flat(),
-  [6, Array(8).fill(null)].flat(),
-  [7, Array(8).fill(bP)].flat(),
-  [8, bR, bN, bB, bQ, bK, bB, bN, bR].flat(),
   [0, "A", "B", "C", "D", "E", "F", "G", "H"],
-].reverse();
+  [8, bR, bN, bB, bQ, bK, bB, bN, bR].flat(),
+  [7, Array(8).fill(bP)].flat(),
+  [6, Array(8).fill(null)].flat(),
+  [5, Array(8).fill(null)].flat(),
+  [4, Array(8).fill(null)].flat(),
+  [3, Array(8).fill(null)].flat(),
+  [2, Array(8).fill(wP)].flat(),
+  [1, wR, wN, wB, wQ, wK, wB, wN, wR].flat(),
+];
 
 export const defaultSquare: Piece = {
   name: null,
   color: null,
   coords: [0, 0],
-  defeated: false,
   moveSet: null,
-}
-
-export interface BoardStore {
-  board: any[][],
-  blackDefeatedPieces: Piece[],
-  whiteDefeatedPieces: Piece[],
-  selectedSquare: Piece | null,
-  isPieceSelected: boolean,
-  turn: TURNS,
-  setBoard: (board: any[][]) => void,
-  setBlackDefeatedPieces: (blackDefeatedPieces: Piece[]) => void,
-  setWhiteDefeatedPieces: (whiteDefeatedPieces: Piece[]) => void,
-  setSelectedSquare: (selectedSquare: Piece | null) => void,
-  setIsPieceSelected: (isPieceSelected: boolean) => void,
-  changeTurn: () => void,
-  reset: () => void,
 }
--- src/types/Global.ts
+++ src/types/Global.ts
@@ -1,10 +1,28 @@
 import { Piece, TURNS } from "./Piece";
 
+export interface Player {
+    score: number,
+    defeatedPieces: Piece[],
+    time: number,
+}
+
 export interface InitialState {
     board: any[][],
-    blackDefeatedPieces: Piece[],
-    whiteDefeatedPieces: Piece[],
+    [TURNS.WHITE]: Player,
+    [TURNS.BLACK]: Player,
     selectedSquare: Piece | null,
-    isPieceSelected: boolean,
+    hoverSquare: Piece | null,
     turn: TURNS,
+    history: any[],
+}
+
+export interface GameStore extends InitialState {
+    setBoard: (board: any[][]) => void,
+    setDefeatedPieces: (color: TURNS, newDefeatedPieces: Piece) => void,
+    setScore: (color: TURNS, points: number) => void,
+    getScore: (color: TURNS) => number,
+    setSelectedSquare: (selectedSquare: Piece | null) => void,
+    setHoverSquare: (hoverSquare: Piece | null) => void,
+    changeTurn: () => void,
+    setHistory: (history: any[]) => void,
 }
