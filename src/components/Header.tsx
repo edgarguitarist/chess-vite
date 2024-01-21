4311e452d5e749213b58ed6bcb0e948c48c6f0bb
edgarguitarist <edgarguitarist@gmail.com>
2024-01-21 12:51:37 -0500

    fix: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,6 +22,7 @@ export default function Header() {
   return (
     <div className="h-[10vh] bg-slate-800 ring-1 ring-slate-900/5 shadow-xl flex justify-between items-center px-10">
       <span className="text-3xl font-bold text-white">AJEDREZ</span>
+      <div className="grid grid-flow-col gap-x-4">
       <button
         type="button"
         className="py-2.5 px-5 text-sm font-medium text-gray-900 bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 hover:scale-105 hover:font-semibold"
@@ -29,6 +30,14 @@ export default function Header() {
       >
         {buttonActionName}
       </button>
+      <button
+        type="button"
+        className="py-2.5 px-5 text-sm font-medium text-gray-900 bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 hover:scale-105 hover:font-semibold"
+        onClick={handleClick}
+      >
+        CARGAR PARTIDA
+      </button>
+      </div>
     </div>
   );
 }
