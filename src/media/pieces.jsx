d8ea8b3a279d52a60678b762bad1e4ed227f8c65
edgarguitarist <edgarguitarist@gmail.com>
2024-01-22 11:16:58 -0500

    Add data-color attribute to SVG elements

--- src/media/pieces.jsx
+++ src/media/pieces.jsx
@@ -5,6 +5,7 @@ export const PIECES = {
       version="1.1"
       width="45"
       height="45"
+      data-color="white"
     >
       <path
         d="m 22.5,9 c -2.21,0 -4,1.79 -4,4 0,0.89 0.29,1.71 0.78,2.38 C 17.33,16.5 16,18.59 16,21 c 0,2.03 0.94,3.84 2.41,5.03 C 15.41,27.09 11,31.58 11,39.5 H 34 C 34,31.58 29.59,27.09 26.59,26.03 28.06,24.84 29,23.03 29,21 29,18.59 27.67,16.5 25.72,15.38 26.21,14.71 26.5,13.89 26.5,13 c 0,-2.21 -1.79,-4 -4,-4 z"
@@ -30,6 +31,7 @@ export const PIECES = {
       version="1.1"
       width="45"
       height="45"
+      data-color="white"
     >
       <g
         style={{
@@ -77,6 +79,7 @@ export const PIECES = {
       version="1.1"
       width="45"
       height="45"
+      data-color="white"
     >
       <g
         style={{
@@ -119,6 +122,7 @@ export const PIECES = {
       version="1.1"
       width="45"
       height="45"
+      data-color="white"
     >
       <g
         style={{
@@ -155,6 +159,7 @@ export const PIECES = {
       version="1.1"
       width="45"
       height="45"
+      data-color="white"
     >
       <g
         style={{
@@ -185,6 +190,7 @@ export const PIECES = {
       version="1.1"
       width="45"
       height="45"
+      data-color="white"
     >
       <g
         style={{
@@ -242,6 +248,7 @@ export const PIECES = {
       version="1.1"
       width="45"
       height="45"
+      data-color="black"
     >
       <path
         d="m 22.5,9 c -2.21,0 -4,1.79 -4,4 0,0.89 0.29,1.71 0.78,2.38 C 17.33,16.5 16,18.59 16,21 c 0,2.03 0.94,3.84 2.41,5.03 C 15.41,27.09 11,31.58 11,39.5 H 34 C 34,31.58 29.59,27.09 26.59,26.03 28.06,24.84 29,23.03 29,21 29,18.59 27.67,16.5 25.72,15.38 26.21,14.71 26.5,13.89 26.5,13 c 0,-2.21 -1.79,-4 -4,-4 z"
@@ -267,6 +274,7 @@ export const PIECES = {
       version="1.1"
       width="45"
       height="45"
+      data-color="black"
     >
       <g
         style={{
@@ -361,6 +369,7 @@ export const PIECES = {
       version="1.1"
       width="45"
       height="45"
+      data-color="black"
     >
       <g
         style={{
@@ -407,6 +416,7 @@ export const PIECES = {
       version="1.1"
       width="45"
       height="45"
+      data-color="black"
     >
       <g
         style={{
@@ -443,6 +453,7 @@ export const PIECES = {
       version="1.1"
       width="45"
       height="45"
+      data-color="black"
     >
       <g
         style={{
@@ -484,6 +495,7 @@ export const PIECES = {
       version="1.1"
       width="45"
       height="45"
+      data-color="black"
     >
       <g
         style={{
