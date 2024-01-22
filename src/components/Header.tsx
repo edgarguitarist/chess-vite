6b5e4c5a5e213d46b510aba1191f397a6077f744
edgarguitarist <edgarguitarist@gmail.com>
2024-01-22 11:18:46 -0500

    Fix button action in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,24 +19,28 @@ export default function Header() {
     }
   };
 
+  const handleClick2 = () => {
+    setStateGame(STATES_GAME.CHECKMATE);
+  };
+
   return (
     <div className="h-[10vh] bg-slate-800 ring-1 ring-slate-900/5 shadow-xl flex justify-between items-center px-10">
       <span className="text-3xl font-bold text-white">AJEDREZ</span>
       <div className="grid grid-flow-col gap-x-4">
-      <button
+        <button
+          type="button"
+          className="py-2.5 px-5 text-sm font-medium text-gray-900 bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 hover:scale-105 hover:font-semibold"
+          onClick={handleClick}
+        >
+          {buttonActionName}
+        </button>
+        {/* <button
         type="button"
         className="py-2.5 px-5 text-sm font-medium text-gray-900 bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 hover:scale-105 hover:font-semibold"
-        onClick={handleClick}
-      >
-        {buttonActionName}
-      </button>
-      <button
-        type="button"
-        className="py-2.5 px-5 text-sm font-medium text-gray-900 bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 hover:scale-105 hover:font-semibold"
-        onClick={handleClick}
+        onClick={handleClick2}
       >
         CARGAR PARTIDA
-      </button>
+      </button> */}
       </div>
     </div>
   );
